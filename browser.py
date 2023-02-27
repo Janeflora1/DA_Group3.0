92274f38aecd09fe8c3456f14ff67b982b838448
Janeflora1 <laywen0618@gmail.com>
2023-02-27 10:31:00 +0800

    Initial commit

---
+++ browser.py
@@ -0,0 +1,37 @@
+# Lab 10b
+
+# Use the Request library
+import requests
+
+# Set the target webpage
+url = 'http://www.wikipedia.org'
+
+
+# Perform a GET request on the target website
+webpage = requests.get(url)
+# This will print the full webpage in text
+print(webpage.text)
+
+
+# Display an "OK" return status code
+print("Status code:")
+print("\t *", webpage.status_code)
+
+
+# Display the website header
+h = requests.head(url)
+print("Header:")
+print("**********")
+# To print line by line
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("**********")
+
+
+# Modify the Header user-agent to display "mobile"
+headers = {'User-Agent': 'Mobile'}
+# Test against test site that output the requester user-agent
+#url2 = 'http://httpbin.org/headers'
+url2 = 'http://172.18.58.80/headers.php'
+request_header = requests.get(url2, headers=headers)
+print(request_header.text)
